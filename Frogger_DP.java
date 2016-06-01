8aa63795a441da5ec269fb9025054ae267fad0c1
asingh2018 <asingh2018@student.amsacs.org>
2016-06-01 14:00:11 -0400

    Update Frogger_DP.java

--- Frogger_DP.java
+++ Frogger_DP.java
@@ -8,12 +8,14 @@ public class Frogger_DP
 	public static final int HEIGHT = 600;
 	public static final int START = 1;
 	public static final int END = 5;
+	public static final int SIZE = 50;
 	public static void main(String[] args) 
 	{
 		DrawingPanel panel = new DrawingPanel(WIDTH, HEIGHT);
 		Graphics g = panel.getGraphics();
 		gameboard(panel, g);
 		cars(panel, g);
+		frog(panel, g);
 	}
 	//This method creates the frogger gameboard
 	public static void gameboard(DrawingPanel panel, Graphics g) 
@@ -28,7 +30,7 @@ public class Frogger_DP
 		}
 		g.setColor(Color.GREEN);
 		//this is wrong
-		g.drawRect(X, Y * 3, WIDTH, HEIGHT/10);
+		g.drawRect(X, Y * 4, WIDTH, HEIGHT/10);
 		g.setColor(Color.YELLOW);
 		for (int i = 0; i <= 16; i+=2) 
 		{
@@ -37,23 +39,32 @@ public class Frogger_DP
 		g.setColor(Color.GREEN);
 		g.drawLine(X, Y, WIDTH, Y);
 	}
-	//This method creates and controls the movement of the cars on the gameboard
+	//This method creates the cars on the gameboard
 	public static void cars(DrawingPanel panel, Graphics g) 
 	{
 	
 		g.setColor(Color.RED);
 		for (int i = START; i <=END; i+=2) 
 		{
-			g.drawRect(50 * i, 500, 50, 25);
+			g.drawRect(SIZE * i, SIZE * 10, SIZE, SIZE/2);
 		}
 		for (int i = START; i <=END; i+=2) 
 		{
-			g.drawRect(50 * i, 250, 50, 25);
+			g.drawRect(SIZE * i, SIZE * 7, SIZE, SIZE/2);
 		}
 		for (int i = START; i <=END; i+=2) 
 		{
-			g.drawRect(50 * i, 50, 50, 25);
+			g.drawRect(SIZE * i, SIZE * 4, SIZE, SIZE/2);
 		}
-		
+		for (int i = START; i <=END; i+=2) 
+		{
+			g.drawRect(SIZE * i, SIZE * 2, SIZE, SIZE/2);
+		}
+	}
+	//This method creates the frog
+	public static void frog(DrawingPanel panel, Graphics g) 
+	{
+		g.setColor(Color.GREEN);
+		g.drawOval(150, 550, SIZE, SIZE);
 	}
 }
